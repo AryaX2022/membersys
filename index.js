34b3ae28174c72fe29c60d7fefd4cdcbfe5b3882
admin <6983299@qq.com>
2023-09-18 16:38:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const cors = require('cors');
+app.use(cors());
+
+var con = mysql.createConnection({
+    host: "13.212.78.127",
+    user: "web",
+    password: "a!r56Z@+",
+    database: "ibooks"
+});
+con.connect(function(err) {
+    if (err) throw err;
+});
+
+app.get('/list', async function (request, response) {
+    con.query("SELECT * FROM books", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    response.json({ret:0});
+});
+
+app.listen(8080, () => console.log(('listening :)')))
